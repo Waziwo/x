352324118b0b88c130c3849a5fb4466bbedc34a8
Waziwo <166222021+Waziwo@users.noreply.github.com>
2024-11-08 22:48:53 +0100

    1

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const btn = document.querySelector('.btn');
-    btn.addEventListener('click', function() {
-        alert('Dziękujemy za zainteresowanie GymStats Pro!');
+    const features = document.querySelectorAll('.feature');
+
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    features.forEach(feature => {
+        observer.observe(feature);
     });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const heroContent = document.querySelector('.hero-content');
+
+    // Ustawienie opóźnienia na pojawienie się
+    setTimeout(() => {
+        heroContent.classList.add('visible');
+    }, 500); // Opóźnienie 0.5 sekundy
 });
